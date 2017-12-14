faff91d25c3421ac9732cae1f1e65d73088ec65f
Debian Med Packaging Team <debian-med-packaging@lists.alioth.debian.org>
2017-12-14 15:09:29 +0000

    tests-pluginpath
    Gbp-Pq: Name tests-pluginpath.patch

--- test/test.pl
+++ test/test.pl
@@ -885,7 +885,7 @@ sub test_vcf_plugin
 {
     my ($opts,%args) = @_;
     if ( !$$opts{test_plugins} ) { return; }
-    $ENV{BCFTOOLS_PLUGINS} = "$$opts{bin}/plugins";
+    $ENV{BCFTOOLS_PLUGINS} = "$$opts{bin}/plugins:";
     if ( !exists($args{args}) ) { $args{args} = ''; }
     $args{args} =~ s/{PATH}/$$opts{path}/g;
     $args{cmd}  =~ s/{PATH}/$$opts{path}/g;
